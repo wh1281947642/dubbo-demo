dc43139e4f43b4196f89659412cdf6c6a75daf6c
wh <1281947642@qq.com>
2020-01-10 15:10:37 +0800

    dubbo-demo

---
+++ consumer/src/main/java/com/dubbo/consumer/service/ProductService.java
@@ -0,0 +1,13 @@
+package com.dubbo.consumer.service;
+
+public interface ProductService {
+
+    /**
+     * 获得总消费
+     *
+     * @param a
+     * @return
+     */
+    Integer getCost(int a);
+
+}
